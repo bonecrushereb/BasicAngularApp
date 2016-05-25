7ef6fac2c100f26c79953e68a0453207bbc856a3
bonecrushereb <bonecrushereb@gmail.com>
2016-05-25 09:33:54 -0700

    add spHandleError service;

--- app/js/preys/controllers/preys_controller.js
+++ app/js/preys/controllers/preys_controller.js
@@ -1,26 +1,26 @@
 const angular = require('angular');
-var handleError = require('../../lib').handleError;
 var baseUrl = require('../../config').baseUrl;
 
 module.exports = function(app) {
 
-  app.controller('PreysController', ['$http', function($http) {
+  app.controller('PreysController', ['$http', 'spHandleError', function($http, spHandleError) {
     this.preys = [];
-
+    this.errors = [];
     this.getAll = function() {
       $http.get(baseUrl + '/api/preys')
         .then((res) => {
           this.preys = res.data;
-        }, handleError.bind(this));
-    };
+        }, spHandleError(this.errors, 'could not retrive preys'));
+    }.bind(this);
 
-    this.createPrey = () => {
+    this.createPrey = function() {
+      var preyName = this.newPrey.name;
       $http.post(baseUrl + '/api/preys', this.newPrey)
         .then((res) => {
           this.preys.push(res.data);
           this.newPrey = null;
-        }, handleError.bind(this));
-    };
+        }, spHandleError(this.errors, 'could not create prey' + prey.name));
+    }.bind(this);
 
     this.editPrey = (prey) => {
       prey.editing = true;
@@ -36,18 +36,18 @@ module.exports = function(app) {
        }
     };
 
-    this.updatePrey = (prey) => {
+    this.updatePrey = function(prey) {
       $http.put(baseUrl + '/api/preys/' + prey._id, prey)
         .then(() => {
           prey.editing = false;
-        }, handleError.bind(this));
-    };
+        }, spHandleError(this.errors, 'could not update prey' + prey.name));
+    }.bind(this);
 
-    this.removePrey = (prey) => {
+    this.removePrey = function(prey) {
       $http.delete(baseUrl + '/api/preys/' + prey._id)
         .then(() => {
           this.preys.splice(this.preys.indexOf(prey), 1);
-        }, handleError.bind(this));
-    };
+        }, spHandleError(this.errors, 'could not delete prey' + prey.name));
+    }.bind(this);
   }]);
 };
--- app/js/sharks/controllers/sharks_controller.js
+++ app/js/sharks/controllers/sharks_controller.js
@@ -1,26 +1,26 @@
 const angular = require('angular');
-var handleError = require('../../lib').handleError;
 var baseUrl = require('../../config').baseUrl;
 
 module.exports = function(app) {
 
-  app.controller('SharksController', ['$http', function($http) {
+  app.controller('SharksController', ['$http','spHandleError', function($http, spHandleError) {
     this.sharks = [];
-
+    this.errors = [];
     this.getAll = function() {
       $http.get(baseUrl + '/api/sharks')
         .then((res) => {
           this.sharks = res.data;
-        }, handleError.bind(this));
-    };
+        }, spHandleError(this.errors, 'could not retreive sharks'));
+    }.bind(this);
 
-    this.createShark = () => {
+    this.createShark = function() {
+      var sharkName = this.newShark.name;
       $http.post(baseUrl + '/api/sharks', this.newShark)
         .then((res) => {
           this.sharks.push(res.data);
           this.newShark = null;
-        }, handleError.bind(this));
-    };
+        }, spHandleError(this.errors, 'could not create shark' + shark.name));
+    }.bind(this);
 
     this.editShark = (shark) => {
       shark.editing = true;
@@ -36,18 +36,18 @@ module.exports = function(app) {
       }
     };
 
-    this.updateShark = (shark) =>{
+    this.updateShark = function(shark) {
       $http.put(baseUrl + '/api/sharks/' + shark._id, shark)
         .then(() => {
           shark.editing = false;
-        }, handleError.bind(this));
-    };
+        }, spHandleError(this.errors, 'could not update shark' + shark.name));
+    }.bind(this);
 
-    this.removeShark = (shark) => {
+    this.removeShark = function(shark) {
       $http.delete(baseUrl + '/api/sharks/' + shark._id)
         .then(() => {
           this.sharks.splice(this.sharks.indexOf(shark), 1);
-        }, handleError.bind(this));
-    };
+        }, spHandleError(this.errors, 'chould not delete this shark' + shark.name));
+    }.bind(this);
   }]);
 };
